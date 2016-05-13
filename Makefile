883eb7b8a5232c459a7b14eeee90cee00f79c7d1
Joao Sallaberry <joaovictorhs@gmail.com>
2016-05-13 00:09:00 -0300

    bugfix: lasers stopped each other
    added debug prints

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	g++ -std=c++11 laser_maze.cpp -o laser_maze
--- laser_maze.cpp
+++ laser_maze.cpp
@@ -1,4 +1,4 @@
-//https://www.facebook.com/hackercup/problem/1523599254559737/
+// https://www.facebook.com/hackercup/problem/1523599254559737/
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -6,8 +6,7 @@
 
 using namespace std;
 
-using maze_t = vector<vector<char>>;
-using square_t = pair<int, int>;
+const bool DEBUG = false;
 
 const char EMPTY = '.';
 const char WALL = '#';
@@ -15,17 +14,19 @@ const char LEFT_TURRET = '<';
 const char RIGHT_TURRET = '>';
 const char UP_TURRET = '^';
 const char DOWN_TURRET = 'v';
+const char START = 'S';
+const char GOAL = 'G';
 
 const char LASER = '*';
 
-void print_maze(const maze_t& maze) {
-    for (const auto& r : maze) {
-	for (const auto& c : r) {
-	    cout << c;
-	}
-	cout << endl;
-    }
-    cout << endl;
+enum moves_t {MOVE_UP, MOVE_DOWN, MOVE_LEFT, MOVE_RIGHT};
+using maze_t = vector<vector<char>>;
+using square_t = pair<int, int>;
+using path_t = pair<square_t, vector<moves_t>>;
+
+
+inline bool is_free(char c) {
+    return c == EMPTY || c == LASER;
 }
 
 vector<maze_t> get_mazes(const maze_t& original_maze) {
@@ -46,7 +47,7 @@ vector<maze_t> get_mazes(const maze_t& original_maze) {
 		// SHOOT LEFT
 		if (m[i][j] == LEFT_TURRET) {
 		    int k = j - 1;
-		    while (k >= 0 && m[i][k] == EMPTY) {
+		    while (k >= 0 && is_free(m[i][k])) {
 			m[i][k] = LASER;
 			k--;
 		    }
@@ -56,7 +57,7 @@ vector<maze_t> get_mazes(const maze_t& original_maze) {
 		// SHOOT RIGHT
 		if (m[i][j] == RIGHT_TURRET) {
 		    int k = j + 1;
-		    while (k < m[0].size() && m[i][k] == EMPTY) {
+		    while (k < m[0].size() && is_free(m[i][k])) {
 			m[i][k] = LASER;
 			k++;
 		    }
@@ -66,7 +67,7 @@ vector<maze_t> get_mazes(const maze_t& original_maze) {
 		// SHOOT UP
 		if (m[i][j] == UP_TURRET) {
 		    int k = i - 1;
-		    while (k >= 0 && m[k][j] == EMPTY) {
+		    while (k >= 0 && is_free(m[k][j])) {
 			m[k][j] = LASER;
 			k--;
 		    }
@@ -76,7 +77,7 @@ vector<maze_t> get_mazes(const maze_t& original_maze) {
 		// SHOOT DOWN
 		if (m[i][j] == DOWN_TURRET) {
 		    int k = i + 1;
-		    while (k < m.size() && m[k][j] == EMPTY) {
+		    while (k < m.size() && is_free(m[k][j])) {
 			m[k][j] = LASER;
 			k++;
 		    }
@@ -96,11 +97,13 @@ maze_t get_curr_maze(const vector<maze_t>& mazes, int round) {
     return mazes[round%mazes.size()];
 }
 
-void print_me(const maze_t& maze, const square_t& me) {
+void print_maze(const maze_t& maze, const square_t& me, const square_t& goal) {
     for (size_t i = 0; i < maze.size(); i++) {
 	for (size_t j = 0; j < maze[0].size(); j++) {
 	    if (me.first == i && me.second == j)
 		cout << 'X';
+	    else if (goal.first == i && goal.second == j)
+		cout << 'G';
 	    else
 		cout << maze[i][j];
 	}
@@ -109,43 +112,90 @@ void print_me(const maze_t& maze, const square_t& me) {
     cout << endl;
 }
 
+void print_path(const maze_t& original_maze, const vector<maze_t>& mazes, const square_t& s, const square_t& g, const vector<moves_t>& moves) {
+    print_maze(original_maze, s, g);
+		
+    square_t sq = s;
+    for (int i = 0; i < moves.size(); i++) {
+	cout << "MOVE " << moves[i] << endl;
+		    
+	switch (moves[i]) {
+	case MOVE_UP:
+	    sq.first--;
+	    break;
+	case MOVE_DOWN:
+	    sq.first++;
+	    break;
+	case MOVE_LEFT:
+	    sq.second--;
+	    break;
+	case MOVE_RIGHT:
+	    sq.second++;
+	    break;
+	}
+
+	print_maze(get_curr_maze(mazes, i + 1), sq, g);
+    }
+}
+
 int find_shortest_path(const maze_t& original_maze, const square_t& s, const square_t& g) {
     vector<maze_t> mazes = get_mazes(original_maze);
-    queue<pair<square_t, int>> to_visit;
+    queue<path_t> to_visit;
     set<pair<square_t, int>> visited;
 
-    to_visit.push({s, 0});
+    to_visit.push({s, vector<moves_t>()});
 
     while (!to_visit.empty()) {
-	pair<square_t, int> curr_sq = to_visit.front();
+	path_t curr_sq = to_visit.front();
 	to_visit.pop();
 
-	maze_t maze = get_curr_maze(mazes, curr_sq.second);
+	maze_t maze = get_curr_maze(mazes, curr_sq.second.size());
 
-	// is move possible?
 	int curr_x = curr_sq.first.first;
 	int curr_y = curr_sq.first.second;
+	
+	// ignore move if out of maze
 	if (curr_x < 0 || curr_x >= maze.size() ||
-	    curr_y < 0 || curr_y >= maze[0].size() ||
-	    visited.count({curr_sq.first, curr_sq.second%mazes.size()}) ||
-	    (maze[curr_x][curr_y] != EMPTY &&
-	     curr_sq.first != s)) // skips check on starting point
+	    curr_y < 0 || curr_y >= maze[0].size())
 	    continue;
 
-	// set visited
-	visited.insert({curr_sq.first, curr_sq.second%mazes.size()});
+	// ignore move if blocked or dead
+	if (maze[curr_x][curr_y] != EMPTY &&
+	    (curr_sq.first != s || to_visit.size())) // skips check on starting point
+	    continue;
 	
-	//print_me(maze, curr_sq.first);
+	// ignore move if state is already visited
+	if (visited.count({curr_sq.first, curr_sq.second.size()%mazes.size()}))
+	    continue;
+
+	// set visited state
+	visited.insert({curr_sq.first, curr_sq.second.size()%mazes.size()});
 
 	// found goal
-	if (curr_sq.first == g)
-	    return curr_sq.second;
+	if (curr_sq.first == g) {
+	    if (DEBUG) {
+		print_path(original_maze, mazes, s, g, curr_sq.second);
+	    }
+
+	    return curr_sq.second.size();
+	}
 
 	// visit neighbours
-	to_visit.push({{curr_sq.first.first + 1, curr_sq.first.second}, curr_sq.second + 1});
-	to_visit.push({{curr_sq.first.first, curr_sq.first.second + 1}, curr_sq.second + 1});
-	to_visit.push({{curr_sq.first.first - 1, curr_sq.first.second}, curr_sq.second + 1});
-	to_visit.push({{curr_sq.first.first, curr_sq.first.second - 1}, curr_sq.second + 1});
+	path_t move_up = {{curr_sq.first.first - 1, curr_sq.first.second}, curr_sq.second};
+	move_up.second.push_back(MOVE_UP);
+	to_visit.push(move_up);
+	
+	path_t move_down = {{curr_sq.first.first + 1, curr_sq.first.second}, curr_sq.second};
+	move_down.second.push_back(MOVE_DOWN);
+	to_visit.push(move_down);
+
+	path_t move_left = {{curr_sq.first.first, curr_sq.first.second - 1}, curr_sq.second};
+	move_left.second.push_back(MOVE_LEFT);
+	to_visit.push(move_left);
+
+	path_t move_right = {{curr_sq.first.first, curr_sq.first.second + 1}, curr_sq.second};
+	move_right.second.push_back(MOVE_RIGHT);
+	to_visit.push(move_right);
     }
 
     return 0;
@@ -167,13 +217,13 @@ int main() {
 		char c;
 		cin >> c;
 
-		if (c == 'S') { s = {i, j}; c = EMPTY; }
-		if (c == 'G') { g = {i, j}; c = EMPTY; }
+		if (c == START) { s = {i, j}; c = EMPTY; }
+		if (c == GOAL) { g = {i, j}; c = EMPTY; }
 	      		
 		maze[i][j] = c;
 	    }
 	}
-
+	
 	int shortest = find_shortest_path(maze, s, g);
 	cout << "Case #" << t << ": ";
 	if (shortest)
